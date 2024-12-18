a4a35a735dff9b7be432ab2c97adfdd49f96ceb1
James Zhao <hjz@hackjumpzero.ca>
2024-12-17 19:38:21 -0500

    update work

--- lua/semhl.lua
+++ lua/semhl.lua
@@ -17,13 +17,16 @@ function ts_diff(start_ts, end_ts)
   return sec .. "." .. pad .. nsec
 end
 
-local LOGGER = require("plenary.log").new({ plugin = PLUGIN_NAME, level = "debug", outfile="semhl.log", use_console = "sync" })
+local LOGGER = require("plenary.log").new({
+  plugin = PLUGIN_NAME,
+  level = "debug",
+  outfile = "semhl.log",
+})
 
 local function create_highlight(ns, rgb_hex)
   rgb_hex = rgb_hex:lower()
   local cache_key = table.concat({ "sfg", rgb_hex }, "_")
   local highlight_name = M._HIGHLIGHT_CACHE[cache_key]
-
 
   -- Look up in our cache.
   if highlight_name then
@@ -40,142 +43,73 @@ local function create_highlight(ns, rgb_hex)
   return highlight_name
 end
 
-local function recursive_child_iter(node, table_to_insert, desired_types)
-  if node:iter_children() then
-    for child in node:iter_children() do
-      if desired_types then
-        if vim.tbl_contains(desired_types, child:type()) then
-          table.insert(table_to_insert, child)
-        end
-      else
-        table.insert(table_to_insert, child)
+local function highlight_node(buffer, node_text, srow, scol, erow, ecol, create_new)
+  local hlname = M._WORD_CACHE[node_text]
+
+  -- if not create_new then
+  --   LOGGER.info("processing: " .. node_text .. "[" .. srow .. "," .. erow .. "] ")
+  -- end
+
+  -- Only create new highlight if create_new is true
+  if hlname == nil and create_new then
+    local random_range = 1000;
+    local hsv = { math.random(0, random_range) / random_range, math.random(0, random_range) / random_range, math
+    .random(0, random_range) / random_range }
+    local c = require("color_generator").color_generate(hsv[1], hsv[2], hsv[3])
+    hlname = create_highlight(M._ns, string.sub(c, 2))
+  end
+
+  if hlname then
+    -- Find NS for diag and make sure there isn't extmark exist for the same range already
+    -- local diag_idx = next(vim.diagnostic.get_namespaces())
+    -- local existing_extmark = nil
+    -- if diag_idx then
+    --   local diag_ns = vim.diagnostic.get_namespaces()[diag_idx].user_data.underline_ns
+    --   if diag_ns then
+    --     existing_extmark = vim.api.nvim_buf_get_extmarks(buffer, diag_ns, { srow, scol }, { erow, ecol }, {})
+    --   end
+    -- end
+    local existing_extmark = vim.api.nvim_buf_get_extmarks(buffer, M._ns, { srow, scol }, { erow, ecol }, {})
+
+    if existing_extmark == nil or next(existing_extmark) == nil then
+      if not create_new then
+        LOGGER.info("ADDING: " ..
+        buffer .. " - " .. node_text .. "[" .. srow .. "," .. scol .. "," .. ecol .. "] " .. hlname)
       end
-
-      recursive_child_iter(child, table_to_insert, desired_types)
+      vim.api.nvim_buf_set_extmark(buffer, M._ns, srow, scol, { end_col = ecol, hl_group = hlname })
     end
+
+    M._WORD_CACHE[node_text] = hlname
   end
 end
 
-local function get_nodes_in_array(buffer) --{{{
-  local ts = vim.treesitter
-
-  -- Yanked from https://github.com/nvim-treesitter/nvim-treesitter/blob/32e364ea3c99aafcce2ce735fe091618f623d889/lua/nvim-treesitter/parsers.lua#L4-L21
-  local filetype_to_parsername = {
-    proto = "c",
-    arduino = "cpp",
-    javascriptreact = "javascript",
-    ecma = "javascript",
-    jsx = "javascript",
-    PKGBUILD = "bash",
-    html_tags = "html",
-    typescriptreact = "tsx",
-    ["typescript.tsx"] = "tsx",
-    terraform = "hcl",
-    ["html.handlebars"] = "glimmer",
-    systemverilog = "verilog",
-    cls = "latex",
-    sty = "latex",
-    OpenFOAM = "foam",
-    pandoc = "markdown",
-    rmd = "markdown",
-    cs = "c_sharp",
-  }
-
-  local ok, parser = pcall(ts.get_parser, buffer)
-  if not ok then
-    local cur_buf_filetype = vim.bo[buffer].ft
-    parser = ts.get_parser(0, filetype_to_parsername[cur_buf_filetype])
+local function process_range(start_row, end_row, parser, tree, buffer, create_new)
+  local query = vim.treesitter.query.parse(parser:lang(), "(identifier) @id")
+  for _, node in query:iter_captures(tree:root(), buffer, start_row, end_row) do
+    local row1, col1, row2, col2 = node:range() -- range of the capture
+    local node_text = vim.treesitter.get_node_text(node, buffer)
+    highlight_node(buffer, node_text, row1, col1, row2, col2, create_new)
   end
-
-  if not parser then
-    return nil
-  end
-
-  local trees = parser:parse()
-  return trees[1]:root()
-end --}}}
-
-local function _on_text_change(buffer)
-  LOGGER.debug("func: _on_text_change: " .. buffer);
-end
-
-local function _on_buffer_hide(buffer)
-  LOGGER.debug("func: _on_buffer_hide: " .. buffer);
 end
 
 local function _new_on_buffer_enter(buffer)
   LOGGER.info("Buffer enter")
   local parser = vim.treesitter.get_parser(buffer, nil)
 
-  local function on_tree_change(range, tree)
-    LOGGER.info("on_tree_change")
-    LOGGER.info(range)
+  local function on_bytes(bufno, tick, srow, scol, sbyte, oerow, oecol, oebyte, nerow, necol, nebyte)
+    local tree = parser:parse()[1]
+    local start_ts = vim.uv.clock_gettime("realtime")
 
-    local children = {}
-    recursive_child_iter(tree:root(), children, { "identifier", "type_identifier", "field_identifier" })
+    process_range(srow, srow + oerow, parser, tree, bufno, false)
 
-    for _, nn in ipairs(children) do
-      local node_text = vim.treesitter.get_node_text(nn, buffer)
-    end
+    local end_ts = vim.uv.clock_gettime("realtime")
+    LOGGER.debug("Diff run took " .. ts_diff(start_ts, end_ts) .. " sec")
   end
 
-  parser:register_cbs({ on_changedtree = on_tree_change }, true)
-end
-
-local function _on_buffer_enter(buffer)
-  LOGGER.debug("func: _on_buffer_enter: " .. buffer);
-  -- require'plenary.profile'.start("semhl_profile.log")
-  -- require'plenary.profile'.stop()
-
-  local load_start_ts = vim.uv.clock_gettime("realtime")
-  local root = get_nodes_in_array(buffer)
-  if not root then
-    return
-  end
-
-  vim.api.nvim_buf_clear_namespace(buffer, M._ns, 0, -1)
-
-  local children = {}
-  recursive_child_iter(root, children, { "identifier", "type_identifier", "field_identifier" })
-
-  for _, nn in ipairs(children) do
-    local node_text = vim.treesitter.get_node_text(nn, buffer)
-    if node_text then
-      local hlname = M._WORD_CACHE[node_text]
-      if hlname == nil then
-        local random_range = 1000;
-        local hsv = { math.random(0, random_range) / random_range, math.random(0, random_range) / random_range, math
-        .random(0, random_range) / random_range }
-        local c = require("color_generator").color_generate(hsv[1], hsv[2], hsv[3])
-        hlname = create_highlight(M._ns, string.sub(c, 2))
-      end
-
-      local srow, scol, erow, ecol = vim.treesitter.get_node_range(nn)
-
-      -- Find NS for diag and make sure there isn't extmark exist for the same range already
-      local diag_idx = next(vim.diagnostic.get_namespaces())
-      local existing_extmark = nil
-      if diag_idx then
-        local diag_ns = vim.diagnostic.get_namespaces()[diag_idx].user_data.underline_ns
-        if diag_ns then
-          existing_extmark = vim.api.nvim_buf_get_extmarks(buffer, diag_ns, { srow, scol }, { erow, ecol }, {})
-        end
-      end
-
-      if existing_extmark == nil or next(existing_extmark) == nil then
-        vim.api.nvim_buf_add_highlight(buffer, M._ns, hlname, srow, scol, ecol)
-      end
-
-
-      M._WORD_CACHE[node_text] = hlname
-    end
-  end
-
-  -- Activate the highlight
+  local tree = parser:parse()[1]
+  parser:register_cbs({ on_bytes = on_bytes }, true)
+  process_range(nil, nil, parser, tree, buffer, true)
   vim.api.nvim_set_hl_ns(M._ns)
-
-  local load_end_ts = vim.uv.clock_gettime("realtime")
-  LOGGER.debug("load time: ", ts_diff(load_start_ts, load_end_ts))
 end
 
 local function _autoload(ev)

449bbb6d71bda094f0eb3902a7d79ea95b6c66cb
H. James Zhao <hjz@block.xyz>
2026-03-06 11:03:33 -0500

    fix: use overlap extmark cleanup without type filter

--- lua/semhl.lua
+++ lua/semhl.lua
@@ -81,7 +81,6 @@ local function semhl_del_extmarks_in_range(buffer, range)
   local srow, scol, erow, ecol = unpack(range)
   local ok, existing_extmark = pcall(vim.api.nvim_buf_get_extmarks, buffer, M._ns, { srow, scol }, { erow, ecol }, {
     overlap = true,
-    type = "highlight",
   })
   if not ok then
     LOGGER.debug("Failed to get extmarks: " .. tostring(existing_extmark))
